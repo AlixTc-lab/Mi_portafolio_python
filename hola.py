82dd70d4ef985e96c14ae0841d006f4be66100b5
Alitzel <alitrejo650@gmail.com>
2026-03-28 23:39:14 -0600

    Primer commit:primer programa Python

---
+++ hola.py
@@ -0,0 +1,4 @@
+#Este programa imprime uun mensaje de bienvenida  y mi primer programa en python
+print("===Mi primer programa en python===\n")
+print("Hola ,mundo  esta es mi  primera linea de codigo en python\n")
+print("Mi nombre es alitzel y estoy aprendiendo a programar en python")

a3f32fe2d450bc9d05c363f7c0e08dde9ea25bd6
Alitzel <alitrejo650@gmail.com>
2026-03-30 21:31:23 -0600

    Declaración de varibles 1

---
+++ variables.py
@@ -0,0 +1,7 @@
+#apreder a declarar variables en python
+
+nombre = "Juan"#asignar el valor "Juan" a la variable nombre
+edad = 20#asignar el valor 20 a la variable edad
+
+print(nombre)  #imprimir el valor de la variable nombre
+print(edad)#imprimir el valor de la variable edad

f70ba653c6975f4633b186fb1545165ed1d73bcd
Alitzel <alitrejo650@gmail.com>
2026-04-08 20:42:19 -0600

    Mi primera lista

---
+++ Listas.py
@@ -0,0 +1,7 @@
+numbers=[10,5,7,2,2,1]
+print("Contenido de la lista original",numbers)#imprime la lista original 
+numbers[0]=111
+print("Contenido de la lista con cambio:",numbers)
+numbers[1]=numbers[4]
+print("Contenido de la lista con intercambio",numbers)
+print("Longitud de la lista",len(numbers))
